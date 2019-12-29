47e9e442c4d32606e9c6b32e6ed6ce7d9b0d6103
Mik <mik23@protonmail.ch>
2019-12-29 20:06:50 +0530

    put

--- index.js
+++ index.js
@@ -1,48 +1,24 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 
 const app = express();
-
 const port = parseInt(process.env.PORT, 10) || 3000;
 
-app.use(bodyParser.json({ limit: "1mb" }));
-app.use(bodyParser.urlencoded({ limit: "1mb", extended: true }));
-app.use(cors("*"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  try {
-    client.connect(async err => {
-      assert.equal(null, err);
-      const db = client.db(process.env.name);
-      const collection = db.collection("data");
-      const data = await collection.find({}).toArray();
-      const messages = data
-        .filter(i => i.message && i.publish)
-        .map(i => ({
-          message: i.message,
-          name:
-            i.name || nicknames[Math.floor(Math.random() * nicknames.length)]
-        }));
-      res.status(200).json({
-        status: "sucesses",
-        messages
-      });
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: "failure",
-      message: `Ain't vibin 'cause; ${err}`
-    });
-  }
+  res.render("index");
 });
 
 app.get("*", (req, res) => {
-  res.redirect("https://mik1337.github.io");
+  res.redirect("/");
 });
 
 app.post("*", (req, res) => {
-  res.redirect("https://mik1337.github.io/"); // 501 failure
+  res.status(200).json({
+    baby_yoda: "sad",
+    mando: "🔫 angy",
+    message: "This is not the way"
+  });
 });
 
-app.listen(port).then(() => console.log(`Running on port ${port}`));
+app.listen(port);
